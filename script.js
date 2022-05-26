8d9bd606c592ee2526079a11b22a89d8bfa1c281
Abhinav Ashish <55495151+Abhinav7903@users.noreply.github.com>
2022-05-26 17:59:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    
+        const deg = 6;
+        const hr = document.querySelector('#hr');
+        const mn = document.querySelector('#mn');
+        const sc = document.querySelector('#sc');
+        setInterval(()=>{
+            var d= new Date();
+            let day=d.getDay();
+                
+            let hh = d.getHours() * 30;
+            let mm = d.getMinutes() * deg;
+            let ss = d.getSeconds() * deg;
+                        
+            hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+            mn.style.transform = `rotateZ(${mm}deg)`;
+            sc.style.transform = `rotateZ(${ss}deg)`;
+                
+    
+        })
+        
+        // var song=document.getElementById("main");
+        // var x=document.getElementById("fri")
+        
+        var playPromise = main.play();
+        var play=fri.play();
+        
+        var d= new Date();
+        let day=d.getDay();
+        console.log(day);
+        if(day==5){
+        if (playPromise !== undefined) {
+          playPromise.then(_ => {
+            // Automatic playback started!
+            // Show playing UI.
+            
+            main.pause();
+          })
+          .catch(error => {
+            // Auto-play was prevented
+            // Show paused UI.
+          });
+        }}
+        else{
+            if (play !== undefined) {
+          play.then(_ => {
+            // Automatic playback started!
+            // Show playing UI.
+            fri.pause();
+          })
+          .catch(error => {
+            // Auto-play was prevented
+            // Show paused UI.
+          });
+        }
+
+        }
+    
+        
+        
+    
+        
+    
+
+});
+
+
